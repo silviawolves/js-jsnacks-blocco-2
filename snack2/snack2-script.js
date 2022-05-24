af249b7376e134dda05ca8fd412435309c7d01e5
silviawolves <silviacanepa.w@gmail.com>
2022-05-24 15:07:35 +0200

    snack2 - completo

--- snack2/snack2-script.js
+++ snack2/snack2-script.js
@@ -7,26 +7,16 @@ let cognomi = ['Baker', 'Scott', 'Carraway', 'Buchanan', 'Sayre', 'Fitzgerald', 
 console.log(nomi)
 console.log(cognomi)
 
-//creo array dove unisco i due array sopra
-const nomiCognomi = [...nomi, ...cognomi]
-console.log(nomiCognomi)
-
-//creo variabile vuota da usare per operare il risultato che voglio
-let result = ''
-
 //creo array finale, vuoto, dove andranno abbinati casualmente nomi+cognomi
 const listaFake = []
 
-//creo un ciclo che scorra tutti i nomi+cognomi
-for (let i = 0; i < nomiCognomi.length; i++) {
-    console.log(nomiCognomi[i], i)
-    nomiCognomi[i] = Math.floor(Math.random() * nomiCognomi.length)
-    console.log(nomiCognomi[i])
-    
+for (let i = 0; i < 14; i++) {
+    const nomeCognome = nomi[Math.floor(Math.random() * nomi.length)] + " " + cognomi[Math.floor(Math.random() * cognomi.length)]
+    console.log(nomeCognome)
+    listaFake.push(nomeCognome)
 }
 
-
-
+console.log(listaFake)
 
 /*
 Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
