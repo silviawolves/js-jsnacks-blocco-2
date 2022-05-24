a93df778596e6c88130a0e04143f637b8a1195f2
silviawolves <silviacanepa.w@gmail.com>
2022-05-24 15:20:47 +0200

    trovo gli indici pari

--- snack3/snack3-script.js
+++ snack3/snack3-script.js
@@ -1,3 +1,15 @@
 "use-strict"
 
-//Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari
+const numeriInteri = [4, 78, 543, 91, 3, 56, 23, 643, 12, 8, 48, 244, 61, 99, 174, 402]
+console.log(numeriInteri)
+
+for (let i = 0; i < numeriInteri.length; i++) {
+    console.log(numeriInteri[i])
+    let indicePari = i % 2 === 0
+    console.log(numeriInteri[i], indicePari)
+}
+
+
+/*
+Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione (indice) dispari
+*/
