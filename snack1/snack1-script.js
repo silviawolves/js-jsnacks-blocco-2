ef5ff226323ac2c2dcc35fabfcc79c1a1397431c
silviawolves <silviacanepa.w@gmail.com>
2022-05-24 13:34:14 +0200

    snack1 - completo

--- snack1/snack1-script.js
+++ snack1/snack1-script.js
@@ -1,3 +1,12 @@
 "use-strict"
 
 //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+let numeroUtente = prompt('Inserisci un numero!')
+numeroInserito = parseInt(numeroUtente)
+
+if (numeroInserito % 2 === 0) {
+    console.log(numeroInserito)
+} else {
+    console.log(numeroInserito + 1)
+}
