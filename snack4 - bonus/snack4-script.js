89a75c1dd4158adaadd7dd60201f02ff07434231
silviawolves <silviacanepa.w@gmail.com>
2022-05-24 16:52:20 +0200

    snack4 - completo?

--- snack4 - bonus/snack4-script.js
+++ snack4 - bonus/snack4-script.js
@@ -7,18 +7,18 @@ const array2 = [1, 2, 3, 4, 5, 6, 7, 8]
 console.log(array1)
 console.log(array2)
 
-
 //creo un ciclo che controlli tutti gli elementi dell'array più corto
-for (let i = 0; i < array2.length; i++ ) {
-    console.log(array2[i])
+for (let i = 0; i < array2.length; i++) {
 
+    if (array2.length < array1.length) {
+        const numeroAggiunto = Math.floor(Math.random() * 100)
+        array2.push(numeroAggiunto)
+    }
 }
 
-//con ogni ciclo controllo se l'array2 è lungo quanto l'array1
-//se non è lungo uguale, pusho un elemento dentro l'array2
-//ripeto il ciclo finché non sono uguali di lunghezza
-
+console.log(array2)
 
 /*
-Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
+Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
 */
